a7f1ecff949855456720bc801f741424c111fcc7
Adrian <adrian.chacon@tenfouranalytics.com>
2019-09-20 19:47:09 -0700

    initial commit

---
+++ src/components/titlebar.js
@@ -0,0 +1,16 @@
+import { Jumbotron } from 'reactstrap'
+
+import React, { Component } from 'react'
+
+class TitleBar extends Component {
+  render () {
+    return (
+      <Jumbotron>
+        <h1>Borderland Cyber Summit</h1>
+        <p>more info</p>
+      </Jumbotron>
+    )
+  }
+}
+
+export default TitleBar
