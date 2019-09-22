fcb476e6815fab538c0f492fce1072c7b2a6a3c1
Eddy <lynn.sloat@gmail.com>
2019-09-22 13:06:48 -0600

    Jumbotron work

--- src/components/title-bar.js
+++ src/components/title-bar.js
@@ -5,9 +5,16 @@ import React, { Component } from 'react'
 class TitleBar extends Component {
   render () {
     return (
-      <Jumbotron>
-        <h1>Borderland Cyber Summit</h1>
-        <p>more info</p>
+      <Jumbotron class='jumbotron' id='jumbo'>
+        <div class='container'>
+          <h1 class='col-md-9 col-md-offset-2'>
+            <p>Borderland Cyber Summit 2019</p>
+            <p>
+              <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              <a class="btn btn-primary btn-lg" href="#" role="button">Register Now</a>
+            </p>
+          </h1>
+        </div>
       </Jumbotron>
     )
   }
