a27c776d0a78f98b0b19be058e73773063396f2e
Eddy <lynn.sloat@gmail.com>
2019-09-22 17:44:31 -0600

    Linked buttons on jumbotron to scroll and played with some css on it

--- src/components/title-bar.js
+++ src/components/title-bar.js
@@ -1,17 +1,23 @@
 import { Jumbotron } from 'reactstrap'
-
 import React, { Component } from 'react'
+import {
+  Link,
+  Scroll
+} from 'react-scroll';
 
 class TitleBar extends Component {
+
   render () {
     return (
       <Jumbotron class='jumbotron' id='jumbo'>
         <div class='container'>
           <h1 class='col-md-9 col-md-offset-2'>
-            <p>Borderland Cyber Summit 2019</p>
-            <p>
-              <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-              <a class="btn btn-primary btn-lg" href="#" role="button">Register Now</a>
+            <p class='jumbotext'>Borderland Cyber Summit 2019</p>
+            <p class='jumbobuttons'>
+              <Link class="btn btn-primary btn-lg margin-left" href="#" role="button" to='formtitle' activeClass= 'active'
+              smooth={true}>Learn more</Link>
+              <Link class="btn btn-primary btn-lg margin-left" href="#" role="button" to='formtitle' activeClass= 'active'
+              smooth={true}>Register Now</Link>
             </p>
           </h1>
         </div>
