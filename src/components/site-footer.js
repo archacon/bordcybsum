69b06257824140f877b969783ceb45ef0a2f7eef
Eddy <lynn.sloat@gmail.com>
2019-09-22 22:11:05 -0600

    first branch commit fixed footer and styled jumbotron

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
 import { Navbar, Container, NavbarBrand } from 'reactstrap'
+import classnames from 'classnames'
+
+
 
 class SiteFooter extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      prevScrollpos: window.pageYOffset,
+      visible: true
+    };
+
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle () {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+  //Scrolling Navbar hide effect
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  // Remove the event listener when the component is unmount.
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  // Hide or show the menu.
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
+
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollpos > currentScrollPos;
+
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible
+    });
+  };
+
+
   render () {
     return (
-      <div className='fixed-bottom'>
-        <Navbar color='dark' dark>
+      <div  className='fixed-bottom'>
+        <Navbar className={classnames("nav-footer", {
+          "navfooter--hidden": !this.state.visible
+        })} color='light' light expand='md' fixed='bottom'>
           <Container>
             <NavbarBrand>Footer</NavbarBrand>
             <p style={{ color: 'white' }}>
