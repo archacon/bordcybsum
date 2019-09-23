a53d161e5c395c90e42f022eb57448b4608ae743
Eddy <lynn.sloat@gmail.com>
2019-09-22 19:46:54 -0600

    fixed mobile responsiveness for menu and jumbotron

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,6 +46,7 @@ const Layout = ({ children }) => {
       >
         <html lang="en" />
         {/* General tags */}
+        <meta name="viewport" content= "width=device-width, initial-scale=1.0" />
         <meta name="description" content={data.site.siteMetadata.description} />
         <meta name="image" content={data.site.siteMetadata.imageurl} />
         <meta property="og:url" content={data.site.siteMetadata.imageurl} />
