35876339261d37bd5a092487592909ea80b5fad2
Adrian <adrian.chacon@tenfouranalytics.com>
2019-09-23 19:27:17 -0700

    add basic modal structure

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -1,189 +1,156 @@
 import React, { Component } from 'react'
 import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap'
 
-
-//Original form by Adrian
-
-/* class RegistrationForm extends Component {
+class RegistrationForm extends Component {
   render () {
     return (
-      <div className='col-12'>
-        <Form id='registrationform' className='' action='' method='POST'>
-          <FormGroup row>
-            <Label for='firstName' sm={{ size: 2, offset: 2 }}>
-              Name
-            </Label>
-            <Col sm={3}>
-              <Input
-                type='text'
-                name='firstName'
-                id='firstName'
-                placeholder='First'
-              />
-            </Col>
-            <Col sm={3}>
-              <Input
-                type='text'
-                name='lastName'
-                id='lastName'
-                placeholder='Last'
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for='exampleEmail' sm={{ size: 2, offset: 2 }}>
-              Email
-            </Label>
-            <Col sm={6}>
-              <Input
-                type='email'
-                name='email'
-                id='exampleEmail'
-                placeholder='example@domain.com'
-              />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Label for='phoneNumber' sm={{ size: 2, offset: 2 }}>
-              Phone Number
-            </Label>
-            <Col sm={6}>
-              <Input type='tel' name='phoneNumber' id='phoneNumber' />
-            </Col>
-          </FormGroup>
-          <FormGroup>
-            <Button type='submit' color='primary' className='offset-sm-2'>
-              Submit
-            </Button>
-          </FormGroup>
-        </Form>
-      </div>
+      <Form id='registrationform' className='' action='' method='POST'>
+        <h1>BORDERLAND CYBFR SECURITY SUMMIT 2019</h1>
+        <h1>REGISTRATION FORM</h1>
+
+        <h3>Personal Information</h3>
+        <FormGroup row>
+          <Label for='firstName' sm={{ size: 2, offset: 2 }}>
+            Name
+          </Label>
+          <Col sm={3}>
+            <Input
+              type='text'
+              name='firstName'
+              id='firstName'
+              placeholder='First'
+            />
+          </Col>
+          <Col sm={3}>
+            <Input
+              type='text'
+              name='lastName'
+              id='lastName'
+              placeholder='Last'
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for='exampleEmail' sm={{ size: 2, offset: 2 }}>
+            Email Address:
+          </Label>
+          <Col sm={6}>
+            <Input
+              type='email'
+              name='email'
+              id='exampleEmail'
+              placeholder='example@domain.com'
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for='phoneNumber' sm={{ size: 2, offset: 2 }}>
+            Cell Phone:
+          </Label>
+          <Col sm={6}>
+            <Input type='tel' name='phoneNumber' id='phoneNumber' />
+          </Col>
+        </FormGroup>
+        <h2>Badge Information</h2>
+        <FormGroup row>
+          <Label for='badgeName' sm={{ size: 2, offset: 2 }}>
+            BadgeName
+          </Label>
+          <Col sm={6}>
+            <Input type='text' name='badgeName' id='badgeName' />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for='company' sm={{ size: 2, offset: 2 }}>
+            Company/School/Organization Name:
+          </Label>
+          <Col sm={6}>
+            <Input type='text' name='company' id='company' />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for='jobTitle' sm={{ size: 2, offset: 2 }}>
+            Job Title:
+          </Label>
+          <Col sm={6}>
+            <Input type='text' name='jobTitle' id='jobTitle' />
+          </Col>
+        </FormGroup>
+        <FormGroup tag='fieldset' row>
+          <legend className='col-form-label col-sm-2'>
+            <h1>How did you hear about this event?</h1>
+          </legend>
+          <Col sm={10}>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />
+                Email advertising
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />
+                Another event or trade show
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />
+                Social media
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />
+                Word of mouth
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />I was invited by a speaker
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' d />I am participating as a
+                speaker
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />
+                Event website
+              </Label>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type='radio' name='radio' />
+                Other
+              </Label>
+            </FormGroup>
+          </Col>
+        </FormGroup>
+        <FormGroup check>
+          <legend className='col-form-label col-sm-2'>
+            Contact Information Release & Model Release???
+          </legend>
+          <Label check>
+            <Input type='radio' id='yes' name='checkbox' />
+            yes
+          </Label>
+          <Label check>
+            <Input type='radio' id='no' name='checkbox2' />
+            no
+          </Label>
+        </FormGroup>
+        <FormGroup>
+          <Button type='submit' color='primary' className='offset-sm-2'>
+            Submit
+          </Button>
+        </FormGroup>
+      </Form>
     )
   }
 }
-
-export default RegistrationForm
-*/
-
-//Flip card form by Eddy
-const cardContainer = RegistrationForm;
-
-// React component for form inputs
-class CardInput extends Component {
-  render() {
-    return(
-      <fieldset>
-        <input name={this.props.name} id={this.props.id} type={this.props.type || 'text'} placeholder={this.props.placeholder} required />
-      </fieldset>
-    )
-  }
-}
-// React component for textarea
-class CardTextarea extends Component {
-  render() {
-    return(
-      <fieldset>
-        <textarea name={this.props.name} id={this.props.id} placeholder={this.props.placeholder} required ></textarea>
-      </fieldset>
-    )
-  }
-}
-
-// React component for form button
-class CardBtn extends Component {
-  render() {
-    return(
-      <fieldset>
-        <button className={this.props.className} type={this.props.type} value={this.props.value}>{this.props.value}</button>
-      </fieldset>
-    )
-  }
-}
-
-// React component for the front side of the card
-class CardFront extends Component {
-  render() {
-    return(
-      <div className='card-side side-front'>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-xs-6'>
-
-            </div>
-
-            <div className='col-xs-6 side-front-content' id='card-front-image'>
-              <h1>Borderland Cyber Summit 2019</h1>
-
-              <h2>El Paso, TX</h2>
-
-              <p>EPCC Admin Services Center</p>
-
-              <p>Blah Blah Blah</p>
-
-              <p>More Blah Blah Blah</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-// React component for the back side of the card
-class CardBack extends Component {
-  render() {
-    return(
-      <div className='card-side side-back'>
-        <div className='container-fluid'>
-          <h1>Register here!</h1>
-
-          <form formAction='' className='card-form'>
-            <div className='row'>
-              <div className='col-xs-6'>
-                <CardInput name='contactFirstName' id='contactFirstName' type='text' placeholder='Your first name' />
-              </div>
-
-              <div className='col-xs-6'>
-                <CardInput name='contactLastName' id='contactLastName' type='text' placeholder='Your last name' />
-              </div>
-            </div>
-
-            <div className='row'>
-              <div className='col-xs-6'>
-                <CardInput name='contactEmail' id='contactEmail' type='email' placeholder='Your email address' />
-              </div>
-
-              <div className='col-xs-6'>
-                <CardInput name='contactNumber' id='contactNumber' type='text' placeholder='Your phone number' />
-              </div>
-            </div>
-
-            <CardTextarea name='contactMessage' id='contactMessage' placeholder='Your message' />
-
-            <CardBtn className='btn btn-danger' type='submit' value='Send message' />
-          </form>
-
-
-        </div>
-      </div>
-    )
-  }
-}
-
-// React component for the card (main component)
-class RegistrationForm extends Component {
-  render() {
-    return(
-      <div className='card-container'>
-        <div className='card-body'>
-          <CardBack />
-
-          <CardFront />
-        </div>
-      </div>
-    )
-  }
-}
-
 
 export default RegistrationForm
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,19 +13,25 @@ import SpeakersBar from '../components/speakers-bar'
 import Header from '../components/header'
 import SiteFooter from '../components/site-footer'
 import ParallaxBar from '../components/parallax-bar'
+import FormModal from '../components/form-modal'
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
 import '../components/App.css'
 
+import RegistrationForm from '../components/registration-form'
+
 const IndexPage = () => (
-  <Layout my={5}>
-    <Header />
-    <TitleBar />
-    <ParallaxBar index={0} />
-    <SpeakersBar />
-    <EventDetailsBar />
-    <AboutUs />
-    <div style={{ marginBottom: '5em' }} />
-  </Layout>
+  <RegistrationForm />
+  // <Layout my={5}>
+  //   <Header />
+
+  //   <TitleBar />
+  //   <ParallaxBar index={0} />
+  //   <FormModal />
+  //   <SpeakersBar />
+  //   <EventDetailsBar />
+  //   <AboutUs />
+  //   <div style={{ marginBottom: '5em' }} />
+  // </Layout>
 )
 
 export default IndexPage
